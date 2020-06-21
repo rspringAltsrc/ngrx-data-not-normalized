4dc959a21e33440438e45841653936039392bbf8
Roger Spring <roger.spring@gmail.com>
2020-06-20 20:10:10 -0700

    add with skip same as addOneToCache

--- src/app/courses/courses-card-list/courses-card-list.component.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.ts
@@ -1,30 +1,30 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { CourseEntityService } from '../services/course-entity.service';
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CoursesCardListComponent {
 
-    @Input()
-    courses: Course[];
+  @Input()
+  courses: Course[];
 
-    constructor(
-      private courseService: CourseEntityService) {
-    }
+  constructor(
+    private courseService: CourseEntityService) {
+  }
 
-  onDeleteCourse(course:Course) {
+  deleteCourse(courseId: number) {
 
-        this.courseService.delete(course)
-            .subscribe(
-                () => console.log("Delete completed"),
-                err => console.log("Deleted failed", err)
-            );
+    this.courseService.delete(courseId)
+      .subscribe(
+        () => console.log("Delete completed"),
+        err => console.log("Deleted failed", err)
+      );
 
 
   }
--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -1,32 +1,47 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {Course} from '../model/course';
-import {Observable} from 'rxjs';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
-import {map, tap, filter, first} from 'rxjs/operators';
+import { map, tap, filter, first } from 'rxjs/operators';
 import { CourseEntityService } from '../services/course-entity.service';
 
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
 
-    courses$ = this.coursesService.entities$;
-    
-    loaded$ = this.coursesService.loaded$.pipe(
-      tap(loaded => {
-        if (!loaded) {
-          this.coursesService.getAll();
-        }
-      }),
-      filter(loaded => !!loaded),
-      first()
-    )
+  courses$ = this.coursesService.entities$;
 
-    constructor(
-      private coursesService: CourseEntityService) {
-    }
+  loaded$ = this.coursesService.loaded$.pipe(
+    tap(loaded => {
+      if (!loaded) {
+        this.coursesService.getAll();
+      }
+    }),
+    filter(loaded => !!loaded),
+    first()
+  )
+
+  constructor(
+    private coursesService: CourseEntityService) {
+  }
+
+  addPrefilledCourse() {
+    const prefilledCourse: Course = {
+      category: "nothin",
+      description: "Manually Added",
+      longDescription:
+        "This is a prefilled course manually added by click using all of the data services.",
+      id: 2657
+    };
+
+    this.coursesService.add(prefilledCourse, { skip: true });
+    // Shorthand
+    // this.coursesService.addOneToCache(prefilledCourse);
+  }
+
 }
